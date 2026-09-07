87e709a1340902dfc74653b167f2e4264f9b5b76
Andrew <ostapenko.andreik@gmail.com>
2026-09-07 21:02:16 +0300

    Added allure steps for elements, added epics, stories, features and integrated allure step in pages\base_page

--- elements/textarea.py
+++ elements/textarea.py
@@ -1,16 +1,25 @@
+import allure
 from playwright.sync_api import Locator, expect
 
 from elements.base_element import BaseElement
 
 
 class TextArea(BaseElement):
+    @property
+    def type_of(self) -> str:
+        return "textarea"
+
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator("textarea").first
 
-    def fill(self, text: str, nth: int = 0, **kwargs):
-        locator = self.get_locator(nth, **kwargs)
-        locator.fill(text)
+    def fill(self, value: str, nth: int = 0, **kwargs):
+        with allure.step(f'Filling {self.type_of} "{self.name}" with value "{value}"'):
+            locator = self.get_locator(nth, **kwargs)
+            locator.fill(value)
 
-    def check_have_value(self, text: str, nth: int = 0, **kwargs):
-        locator = self.get_locator(nth, **kwargs)
-        expect(locator).to_have_value(text)
+    def check_have_value(self, value: str, nth: int = 0, **kwargs):
+        with allure.step(
+            f'Checking that {self.type_of} "{self.name}" has value "{value}"'
+        ):
+            locator = self.get_locator(nth, **kwargs)
+            expect(locator).to_have_value(value)
--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -1,15 +1,25 @@
 import allure
 import pytest
+from allure_commons.types import Severity
 
 from pages.authentication.login_page import LoginPage
 from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
 from tools.allure.tags import AllureTag
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
 @allure.tag(AllureTag.REGRESSION, AllureTag.AUTHORIZATION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION)
+@allure.story(AllureStory.AUTHORIZATION)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.AUTHENTICATION)
+@allure.sub_suite(AllureStory.AUTHORIZATION)
 class TestAuthorization:
     @pytest.mark.parametrize(
         "email, password",
@@ -20,6 +30,8 @@ class TestAuthorization:
         ],
     )
     @allure.title("User login with wrong email or password")
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.severity(Severity.CRITICAL)
     def test_wrong_email_or_password_authorization(
         self, login_page: LoginPage, email: str, password: str
     ):
@@ -32,6 +44,8 @@ class TestAuthorization:
         login_page.check_visible_wrong_email_or_password_alert()
 
     @allure.title("User login with correct email and password")
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.severity(Severity.BLOCKER)
     def test_successful_authorization(
         self,
         registration_page: RegistrationPage,
@@ -59,6 +73,8 @@ class TestAuthorization:
         dashboard_page.toolbar_view.check_visible()
 
     @allure.title("Navigation from login page to registration page")
+    @allure.tag(AllureTag.NAVIGATION)
+    @allure.severity(Severity.NORMAL)
     def test_navigate_from_authorization_to_registration(
         self, registration_page: RegistrationPage, login_page: LoginPage
     ):
