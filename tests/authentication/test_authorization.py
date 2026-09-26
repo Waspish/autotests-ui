57e81e2c5ab0ab1e630598642712ffd979420f94
Andrew <ostapenko.andreik@gmail.com>
2026-09-26 19:56:58 +0300

    added failing test to check workflow

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -87,3 +87,8 @@ class TestAuthorization:
         registration_page.registration_form.check_visible(
             email="", password="", username=""
         )
+
+    def test_flaky_authorization(
+        self,
+    ):
+        assert False
